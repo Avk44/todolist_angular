10fdae3ad70113e7a49e1bbef4071dc58a781289
Abdulvahap Karabekmez <abdulvahap.karabekmez@gmail.com>
2021-10-06 21:59:26 +0200

    deleted header component and added home component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,15 +3,15 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { HeaderComponent } from './header/header.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MatSliderModule } from '@angular/material/slider';
 import {MatCheckboxModule} from "@angular/material/checkbox";
+import { HomeComponent } from './components/home/home.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeaderComponent
+    HomeComponent
   ],
   imports: [
     BrowserModule,
